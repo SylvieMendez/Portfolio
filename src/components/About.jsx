9fe49bf4e9f3e0768bf8219ee16bbf53fb7e4c9d
sylvie <sylviemendez21@gmail.com>
2025-05-23 12:10:15 -0400

    update about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
 >
         I'm Sylvie — an aspiring Frontend Software Engineer driven by a passion for building more than just functional code—I strive to create digital experiences that are impactful, transformative, and user-focused. I began teaching myself to code two years ago and am now pursuing a degree in Computer Science at Hunter College to deepen my understanding and advance my technical skills.
 
-I specialize in developing modern, responsive interfaces using React, JavaScript, HTML, and CSS, and I have hands-on experience with backend technologies like Node.js and MongoDB. My focus is on writing clean, maintainable code and crafting reusable components that prioritize performance, accessibility, and cross-device compatibility.
+I specialize in developing modern, responsive interfaces using React, JavaScript, Tailwind, HTML, and CSS, and I have hands-on experience with backend technologies like Node.js and MongoDB. My focus is on writing clean, maintainable code and crafting reusable components that prioritize performance, accessibility, and cross-device compatibility.
 
 I’m adept at integrating RESTful APIs, managing application state effectively, and collaborating throughout the development lifecycle to bring intuitive, high-quality products to life. I'm especially passionate about UI/UX design and continually explore emerging frontend technologies to stay ahead in the field.
 
